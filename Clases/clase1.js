05a6655d52f7a20b3878b53875cd9ae5b0e8b602
StalinX <sprt.prof@gmail.com>
2022-07-04 22:51:35 -0500

    Tarea1.1 y Tarea 1.2

---
+++ Clases/clase1.js
@@ -0,0 +1,28 @@
+/* //variables
+40 // number
+"Stalin" //String
+//vacios
+null
+undefined
+//booleanos
+true
+false
+
+
+//[] //Array
+//{} //Objetos
+
+////////////////////////////////////////////
+
+// funciones
+//declarativas y de expresión
+
+function myFunction(){
+    return "hola Stalin";
+}
+
+var anotherFunction = function(a,b){
+    return a + b;
+}
+
+*/
