147687593684f27f062eeeb174619208e956460a
StalinX <sprt.prof@gmail.com>
2022-07-18 22:37:19 -0500

    Callback

--- JavaScript_Tareas/Semana3/src/callback/callenge.js
+++ JavaScript_Tareas/Semana3/src/callback/callenge.js
@@ -16,16 +16,33 @@ function fetchData(urlApi, callback){
             if (xhttp.status === 200){
                 callback(null, JSON.parse(xhttp.responseText))
             }else{
-                const error = new Error('Error '+urlApi);
+                const error = new Error('Error '+ urlApi);
                 callback(error, null)
             }
         }
     }
     //enviar solicitud
     xhttp.send()
-}   
+}
 
+//guardo en memoria
+const newArray = [];
+//llamo a la funcion 
+//asincronismo
 fetchData(`${API}/character`, function(error1, data1){
     if(error1) return console.error(error1);
-    console.log(data1)
+    //console.log(data1);
+    fetchData(`${API}/character/${data1.results[0].id}`, function(error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(`${API}/location/${data2.id}`, function(error3, data3){
+            if (error3) return console.error(error3);
+            newArray.push(data1.info.count);
+            console.log(newArray);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    })      
 })
+//
+console.log(newArray);
