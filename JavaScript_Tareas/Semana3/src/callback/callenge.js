d1971337ba66c3714a2c8dfae900fdf7e202674b
StalinX <sprt.prof@gmail.com>
2022-07-18 21:15:44 -0500

    Semana3

---
+++ JavaScript_Tareas/Semana3/src/callback/callenge.js
@@ -0,0 +1,31 @@
+//Traer el paquete de xmlhttprequest
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+//Generando un String de mi API de Rick
+const API = 'https://rickandmortyapi.com/api';
+
+//Generando una funcion para traer los datos
+function fetchData(urlApi, callback){
+    let xhttp = new  XMLHttpRequest();
+    // abre la conexion API
+    xhttp.open('GET', urlApi, true);
+    //vERIFICA EL ESTADO DE LA CONEXION
+    xhttp.onreadystatechange = function (even) {
+        //validando el estado de la conexion
+        if (xhttp.readyState === 4) {
+            //validar las respuestas del servidor
+            if (xhttp.status === 200){
+                callback(null, JSON.parse(xhttp.responseText))
+            }else{
+                const error = new Error('Error '+urlApi);
+                callback(error, null)
+            }
+        }
+    }
+    //enviar solicitud
+    xhttp.send()
+}   
+
+fetchData(`${API}/character`, function(error1, data1){
+    if(error1) return console.error(error1);
+    console.log(data1)
+})
