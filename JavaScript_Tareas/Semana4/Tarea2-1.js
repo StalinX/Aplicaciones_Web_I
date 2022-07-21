5f1e7cddcc75b5ede163381a3a80981dbb6e0f49
StalinX <sprt.prof@gmail.com>
2022-07-20 19:53:54 -0500

    Cambios

--- JavaScript_Tareas/Semana4/Tarea2-1.js
+++ JavaScript_Tareas/Semana4/Tarea2-1.js
@@ -1,53 +1,25 @@
-import fetch from "node-fetch";
-async function getCharacters() {
-    const response = await fetch(`https://rickandmortyapi.com/api/character`);
-    const data = await response.json();
-    return data.results;
+
+//import fetch from 'node-fetch'
+const API = 'https://rickandmortyapi.com/api';
+async function fetchData(urlAPI){
+  try{
+      const response = await fetch(`${API/character}`)
+      return response.json()
+  } catch(error){
+      return error;
   }
-  
-  // or, mixed
-  async function getCharacters() {
-    const data = await fetch(`https://rickandmortyapi.com/api/character`).then(
-      (res) => res.json()
-    );
-    return data.results;
-  }
-  
-  (async () => {
-    try {
-      let characters = await getCharacters();
-      console.log(characters); // (20) [{...}, {...}, {...}]
-    } catch (err) {
-      console.error(err);
-    }
-  })();
-
-
-
-
-
-
-
-
-
-const API = "https://rickandmortyapi.com/api/character/"
-async function fetchData(endpoint){
-    try{
-        const res = await fetchData(endpoint)
-        return res.json()
-    } catch(err){
-        return err;
-    }
 }
+
+
 async function resolveRequests(){
-    const firstRq = await fetchData(API)
-    console.log( "Number of characters:", firstRq.info.count )
+    const data1 = await fetchData(API)
+    console.log(data1.info.count)
 
-    const secondRq = await fetchData(API + firstRq.results[0].id)
-    console.log( "First character name", secondRq.name )
+    const data2 = await fetchData(API + data1.results[0].id)
+    console.log( data2.name )
 
-    const lastRq = await fetchData(secondRq.origin.url)
-    console.log( "First character origin", lastRq.name )
+    const data3 = await fetchData(data2.origin.url)
+    console.log( data3.dimension )
 }
 
 resolveRequests()
