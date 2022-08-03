96710919a52613b961142848e173b788db6c408e
StalinX <sprt.prof@gmail.com>
2022-08-02 23:36:57 -0500

    Semana 6

---
+++ JavaScript_Tareas/Semana6/src/assets/main.js
@@ -0,0 +1,55 @@
+
+/*
+**
+*** 
+Genera el consumo de una API (la entregada en clases) y mostrar los videos de youtube 
+*/
+
+// URL API 
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCZiyPvz2BTIqkl0j0A_Ae9Q&part=snippet%2Cid&order=date&maxResults=10';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b5c3162638mshe1eec390647d96ap1652efjsn5e63ec3345ce',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+// declarar las varianles 
+const div1 = document.querySelector('#content');
+
+
+// funcion de llamado a nuestra API
+const fetchData = async(urlApi) =>{
+    const response = await fetch(urlApi,options);
+    const data = await response.json();
+    return data;
+}
+// GET Videos
+//autollamado de una funcion
+const getVideos = async () =>{
+    const videos = await fetchData(API);
+    const content = document.getElementById('content');
+    let view = `
+    ${videos.items.map((vi) => `
+    <div class="group relative">
+        <div
+        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${vi.snippet.thumbnails.high.url}" alt="" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+                <span aria-hidden="true" class="absolute inset-0"></span>
+                ${vi.snippet.title}           
+            </h3>
+        </div>
+    </div>`
+    ).splice(0,10).join('')}`;
+    content.innerHTML = view;
+}
+getVideos();
+
+/* fetch('', 
+options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err)); */
